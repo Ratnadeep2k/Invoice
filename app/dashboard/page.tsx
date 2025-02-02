b72b60398f48b25736e59b54ca614ddc09df08a8
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-03 00:26:41 +0530

    Implement sign-out functionality in the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,17 @@
+import { signOut } from "../utils/auth";
 import { requireUser } from "../utils/hooks";
 export default async function Dashboard() {
     const session = await requireUser();
     return (
         <div >
-            <h1>Dashboard</h1>
-            <p>Welcome to the dashboard</p>
+            <form
+                action={async () => {
+                    "use server"
+                    await signOut()
+                }}
+            >
+                <button type="submit">Sign Out</button>
+            </form>
         </div>
     )
 }
