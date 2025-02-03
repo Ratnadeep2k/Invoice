def4cd75f45141a1c85a79472985fd62267be1ed
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-03 21:21:54 +0530

    Add Sheet component and integrate it into the dashboard layout for improved navigation

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,29 +3,53 @@ import { requireUser } from "../utils/hooks"
 import im from '@/public/im.svg'
 import Image from "next/image"
 import { DashBoardLinks } from "../components/DasboardLinks"
+import { Menu } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
 
-export default async function DashboardLayout({children}:{children: React.ReactNode}) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
     const session = await requireUser()
     return (
-       <>
+        <>
             <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-                    <div className="hidden border-r bg-muted/40 md:block">
-                        <div className="flex flex-col gap-2 h-full max-h-screen">
-                            <div className="h-14 flex items-center border-b px-4 lg:h-[60px] lg:px-6">
-                                <Link href="/" className="flex items-center gap-2">
-                                    <Image src={im} alt="Logo" className="size-7" />
-                                    <p className="text-2xl font-bold text-gray-800"> Invoice  <span className=" text-blue-600 "> Manager </span> </p>
-                                </Link>
-                            </div>
-                            <div className="flex-1">
-                                <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                                    <DashBoardLinks/>
-                                </nav>
+                <div className="hidden border-r bg-muted/40 md:block">
+                    <div className="flex flex-col gap-2 h-full max-h-screen">
+                        <div className="h-14 flex items-center border-b px-4 lg:h-[60px] lg:px-6">
+                            <Link href="/" className="flex items-center gap-2">
+                                <Image src={im} alt="Logo" className="size-7" />
+                                <p className="text-2xl font-bold text-gray-800"> Invoice  <span className=" text-blue-600 "> Manager </span> </p>
+                            </Link>
+                        </div>
+                        <div className="flex-1">
+                            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                                <DashBoardLinks />
+                            </nav>
 
-                            </div>
                         </div>
                     </div>
+                </div>
+                <div className=' flex flex-col'>
+                    <header className='flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6'>
+                        <Sheet>
+                           
+                            <SheetTrigger asChild>
+                                <Button variant="outline" size="icon" className="md:hidden">
+                                    <Menu className="size-5" />
+                                </Button>
+                            </SheetTrigger>
+
+    
+                            <SheetContent side="left">
+                                <nav className="grid gap-2 mt-10 ">
+                                    <DashBoardLinks />
+                                </nav>
+                            </SheetContent>
+                        </Sheet>
+
+                    </header>
+
+                </div>
             </div>
-       </>
+        </>
     )
 }
