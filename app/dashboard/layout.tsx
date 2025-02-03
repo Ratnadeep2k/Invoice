6c2e02a7e900de49dfada340f2d41fa733074323
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-03 21:45:28 +0530

    Refactor dashboard layout for improved code readability and consistency

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -33,14 +33,14 @@ export default async function DashboardLayout({ children }: { children: React.Re
                 <div className=' flex flex-col'>
                     <header className='flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6'>
                         <Sheet>
-                           
+
                             <SheetTrigger asChild>
                                 <Button variant="outline" size="icon" className="md:hidden">
                                     <Menu className="size-5" />
                                 </Button>
                             </SheetTrigger>
 
-    
+
                             <SheetContent side="left">
                                 <nav className="grid gap-2 mt-10 ">
                                     <DashBoardLinks />
@@ -52,23 +52,23 @@ export default async function DashboardLayout({ children }: { children: React.Re
                             <DropdownMenu>
                                 <DropdownMenuTrigger asChild>
                                     <Button className="rounded-full" variant='outline' size='icon' >
-                                        <User2/>
+                                        <User2 />
                                     </Button>
                                 </DropdownMenuTrigger>
                                 <DropdownMenuContent align="end">
                                     <DropdownMenuLabel>
                                         My Account
                                     </DropdownMenuLabel>
-                                    <DropdownMenuSeparator/>
+                                    <DropdownMenuSeparator />
                                     <DropdownMenuItem asChild>
                                         <Link href="/dashboard">Dashboard</Link>
                                     </DropdownMenuItem>
                                     <DropdownMenuItem asChild>
                                         <Link href="/dashboard/invoices">Invoices</Link>
                                     </DropdownMenuItem>
-                                    <DropdownMenuSeparator/>
+                                    <DropdownMenuSeparator />
                                     <DropdownMenuItem asChild>
-                                        <form className="w-full" action={async ()=>{
+                                        <form className="w-full" action={async () => {
                                             'use server'
                                             await signOut();
                                         }}>
@@ -81,7 +81,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
                             </DropdownMenu>
 
                         </div>
-             
+
                     </header>
                     <main className="flex flex-1 flex-col  gap-4 p-4 lg:gap-6 lg:p-6">
                         {children}
