f6858c3ebea7ac3b8cb3a873a9b37b5b3c957267
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-04 20:05:54 +0530

    Finish Auth

--- app/utils/zodSchema.ts
+++ app/utils/zodSchema.ts
@@ -2,5 +2,5 @@ import  {z} from 'zod';
 export const onBoardingSchema =z.object({
     firstName:z.string().min(2,'firstname is required').max(10,'should max of 10char').nonempty(),
     lastName:z.string().nonempty().min(2,'lastname is required').max(10,'should max of 10char'),
-    address:z.string().nonempty().min(2,'address is required').max(10,'should max of 30char')
+    address:z.string().nonempty().min(2,'address is required').max(10,'should max of 10char')
 })
