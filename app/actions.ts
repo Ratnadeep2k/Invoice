a6e13a3470e74566a7a60d6ef5e2251ee574dc89
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-08 01:55:40 +0530

    Add invoice deletion functionality with confirmation page

--- app/actions.ts
+++ app/actions.ts
@@ -149,3 +149,16 @@ export async function editInvoice(prevState:any,formData : FormData ){
 
   return redirect("/dashboard/invoices");
 }
+
+export async function DeleteInvoice(invoiceId: string) {
+  const session = await requireUser();
+
+  const data = await prisma.invoice.delete({
+    where: {
+      userId: session.user?.id,
+      id: invoiceId,
+    },
+  });
+
+  return redirect("/dashboard/invoices");
+}
--- app/components/InvoiceActions.tsx
+++ app/components/InvoiceActions.tsx
@@ -45,7 +45,7 @@ export function InvoiceActions({id}:iAppProps) {
                         <Mail className="size-4 mr-2"/ > Send Reminder
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                    <Link href='' >
+                    <Link href={`/dashboard/invoices/${id}/delete`} >
                         <Trash2Icon className="size-4 mr-2"/> Delete Invoice 
                     </Link>
                 </DropdownMenuItem>
