76453d7003fcfd2c9df97b65d84fc6a123aa2555
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-08 19:39:31 +0530

    Add DashboardBlock component to display invoice statistics

---
+++ app/components/DashboardBlocks.tsx
@@ -0,0 +1,95 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+import { prisma } from "../utils/db";
+import { requireUser } from "../utils/hooks";
+import { formateCurrency } from "../utils/formateCurrency";
+
+async function getData(userId: string){
+    const [data,openInvoices,paidInvoices] = await Promise.all([
+        prisma.invoice.findMany({
+            where :{
+                userId: userId,
+            },
+            select:{
+                total: true,
+            }
+        }),
+        prisma.invoice.findMany({
+            where:{
+                userId: userId,
+                status: "PENDING"
+            },
+            select:{
+                id: true,
+            }
+        }),
+        prisma.invoice.findMany({
+            where:{
+                userId: userId,
+                status: "PAID"
+            },
+            select:{
+                id: true,
+            }
+        })
+    ]);
+    return [data,openInvoices,paidInvoices]
+}
+
+
+export async function DashboardBlock(){
+    const session = await requireUser();
+    const [data, openInvoices, paidInvoices] = await getData(session.user?.id as string);
+    return(
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-8" >
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+                    <DollarSign className="size-4 text-muted-foreground"/>
+                </CardHeader>
+                <CardContent>
+                        <h2 className="text-2xl font-bold">
+                            {data.reduce((acc, invoice) => acc + (invoice as {total:number}).total , 0 )
+                            }
+
+                        </h2>
+                        <p className="text-xs text-muted-foreground">Since last month</p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Total Invoices Issued </CardTitle>
+                    <Users className="size-4 text-muted-foreground"/>
+                </CardHeader>
+                <CardContent>
+                        <h2>+{data.length}</h2>
+                        <p className="text-xs text-muted-foreground"> Invoices Issued </p>  
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Paid Invoices </CardTitle>
+                    <CreditCard className="size-4 text-muted-foreground"/>
+                </CardHeader>
+                <CardContent>
+                        <h2>+{paidInvoices.length}</h2>
+                        <p className="text-xs text-muted-foreground">Total Paid Invoice</p>  
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Open Invoices</CardTitle>
+                    <Activity className="size-4 text-muted-foreground"/>
+                </CardHeader>
+                <CardContent>
+                        <h2>+{openInvoices.length}</h2>
+                        <p className="text-xs text-muted-foreground">Unpaid Invoices </p>  
+                </CardContent>
+            </Card>
+
+
+
+
+        </div>
+    )
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,10 @@
-import { signOut } from "../utils/auth";
+import { DashboardBlock } from "../components/DashboardBlocks";
 import { requireUser } from "../utils/hooks";
 export default async function Dashboard() {
     const session = await requireUser();
     return (
-        <div >
-            <form
-                action={async () => {
-                    "use server"
-                    await signOut()
-                }}
-            > 
-                <button type="submit">Sign Out</button>
-            </form>
-        </div>
+       <>
+            <DashboardBlock />
+       </>
     )
 }
