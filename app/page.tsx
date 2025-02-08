1b1315ccaff830aea89ac39354f98c378ed6507a
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-08 23:25:35 +0530

    Add Hero component and RainbowButton for enhanced landing page experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
    <main className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Navbar />
-
+      <Hero/>
    </main>
   );
 }
